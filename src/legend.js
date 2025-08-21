256cf78f36a6da959fbc7983c96d7d9897a77082
Alx <alxndr.dmtrv@outlook.com>
2025-08-22 00:04:05 +0300

    Renamed legend file

--- src/legend.js
+++ src/legend.js
@@ -4,9 +4,9 @@ document.getElementById('close-legend-button').onclick = function() {
 
 document.getElementById('switch-language-legend-button').onclick = function() {
   const iframe = document.getElementById('legend-iframe');
-  if (iframe.src.includes('/legend/bg/cyclosm_legend.html')) {
-    iframe.src = '/legend/en/cyclosm_legend.html';
+  if (iframe.src.includes('/legend/bg/legend.html')) {
+    iframe.src = '/legend/en/legend.html';
   } else {
-    iframe.src = '/legend/bg/cyclosm_legend.html';
+    iframe.src = '/legend/bg/legend.html';
   }
 };
